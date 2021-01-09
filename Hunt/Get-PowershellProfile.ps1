80f6254a0d6f3db5832e96112b179b91177de939
tmenochet <tmenochet@protonmail.com>
2021-01-09 14:08:51 +0100

    Added Get-PowershellProfile

---
+++ Hunt/Get-PowershellProfile.ps1
@@ -0,0 +1,146 @@
+#requires -version 3
+
+function Get-PowershellProfile {
+<#
+.SYNOPSIS
+    Get Powershell profile on a remote computer.
+    Privileges required: high
+
+    Author: Timothée MENOCHET (@_tmenochet)
+
+.DESCRIPTION
+    Get-PowershellProfile enumerates Powershell profile files on a remote host through WMI and optionally downloads them.
+
+.PARAMETER ComputerName
+    Specifies the target host.
+
+.PARAMETER Credential
+    Specifies the privileged account to use.
+
+.PARAMETER Ping
+    Ensures host is up before run.
+
+.PARAMETER Protocol
+    Specifies the protocol to use.
+
+.PARAMETER Download
+    Enables file download.
+
+.EXAMPLE
+    PS C:\> Get-PowershellProfile -ComputerName SRV.ADATUM.CORP -Credential ADATUM\Administrator -Download
+#>
+
+    [CmdletBinding()]
+    Param (
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ComputerName = $env:COMPUTERNAME,
+
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        [Switch]
+        $Ping,
+
+        [ValidateSet('Dcom', 'Wsman')]
+        [String]
+        $Protocol = 'Dcom',
+
+        [Switch]
+        $Download
+    )
+
+    BEGIN {
+        if ($Ping -and -not $(Test-Connection -Count 1 -Quiet -ComputerName $ComputerName)) {
+            Write-Verbose "[$ComputerName] Host is unreachable."
+            break
+        }
+
+        $cimOption = New-CimSessionOption -Protocol $Protocol
+        $psOption = New-PSSessionOption -NoMachineProfile
+        try {
+            if ($Credential.Username) {
+                $cimSession = New-CimSession -ComputerName $ComputerName -Credential $Credential -SessionOption $cimOption -ErrorAction Stop -Verbose:$false
+                if ($Download -and $Protocol -eq 'Wsman') {
+                    $psSession = New-PSSession -ComputerName $ComputerName -Credential $Credential -SessionOption $psOption -ErrorAction Stop -Verbose:$false
+                }
+            }
+            else {
+                $cimSession = New-CimSession -ComputerName $ComputerName -SessionOption $cimOption -ErrorAction Stop -Verbose:$false
+                if ($Download -and $Protocol -eq 'Wsman') {
+                    $psSession = New-PSSession -ComputerName $ComputerName -SessionOption $psOption -ErrorAction Stop -Verbose:$false
+                }
+            }
+        }
+        catch [Microsoft.Management.Infrastructure.CimException] {
+            Write-Verbose "[$ComputerName] Failed to establish CIM session."
+            break
+        }
+        catch [System.Management.Automation.Remoting.PSRemotingTransportException] {
+            Write-Verbose "[$ComputerName] Failed to establish PSRemoting session."
+            break
+        }
+    }
+
+    PROCESS {
+        Get-CimInstance -ClassName Win32_UserProfile -CimSession $cimSession -Verbose:$false | ForEach-Object {
+            $profilePath = $_.LocalPath
+            $profileDrive = ($profilePath -split ":").Get(0)
+            $profileDir = ($profilePath -split ":").Get(1) + "\Documents\WindowsPowershell\" -replace '\\','\\'
+            $filter  = "Drive='${profileDrive}:' AND Path='$profileDir' AND FileName='Microsoft.Powershell_profile' AND Extension='ps1'"
+            $file = Get-CimInstance -ClassName CIM_LogicalFile -Filter $filter -CimSession $cimSession -Verbose:$false
+            if ($file.Name) {
+                $obj = New-Object -TypeName psobject
+                $obj | Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $ComputerName
+                $obj | Add-Member -MemberType NoteProperty -Name 'SID' -Value $_.SID
+                $obj | Add-Member -MemberType NoteProperty -Name 'PowershellProfile' -Value $file.Name
+                $obj | Add-Member -MemberType NoteProperty -Name 'CreationDate' -Value $file.CreationDate
+                $obj | Add-Member -MemberType NoteProperty -Name 'LastModified' -Value $file.LastModified
+                Write-Output $obj
+
+                if ($Download) {
+                    $temp = $profilePath -split "\\"
+                    $userName = $temp.get($temp.Count - 1)
+                    $fileName = "$($file.FileName).$($file.Extension)"
+                    $filePath = "$($file.Path)\$fileName"
+                    $outputDir = "$PWD\$ComputerName"
+                    $outputFile = "$outputDir\${userName}_$fileName"
+                    New-Item -ItemType Directory -Force -Path $outputDir | Out-Null
+                    if ($Protocol -eq 'Wsman') {
+                        # Download file via PSRemoting
+                        Copy-Item -Path "${profileDrive}:$filePath" -Destination $outputFile -FromSession $psSession
+                    }
+                    else {
+                        # Download file via SMB
+                        if ($Credential.Username) {
+                            $drive = Get-StringHash $ComputerName
+                            New-PSDrive -Name $drive -Root "\\$ComputerName\$profileDrive`$" -PSProvider "FileSystem" -Credential $Credential | Out-Null
+                            Copy-Item -Path "${drive}:$filePath" -Destination $outputFile
+                            Remove-PSDrive $drive
+                        }
+                        else {
+                            Copy-Item -Path "\\$ComputerName\$profileDrive`$$filePath" -Destination $outputFile
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    END {
+        Remove-CimSession -CimSession $cimSession
+        if ($Download -and $Protocol -eq 'Wsman') {
+            Remove-PSSession -Session $psSession
+        }
+    }
+}
+
+function Local:Get-StringHash ([String]$String, $Algorithm="MD5") { 
+    $stringBuilder = New-Object System.Text.StringBuilder 
+    [System.Security.Cryptography.HashAlgorithm]::Create($Algorithm).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String)) | % { 
+        [Void]$stringBuilder.Append($_.ToString("x2")) 
+    } 
+    $stringBuilder.ToString() 
+}

247a46cf875a76c48d78e87e1c4a1f0ecb4f5371
tmenochet <tmenochet@protonmail.com>
2021-01-09 08:10:26 +0100

    Added exclusion check

--- Hunt/Get-SecurityStatus.ps1
+++ Hunt/Get-SecurityStatus.ps1
@@ -66,15 +66,16 @@ function Get-SecurityStatus {
             break
         }
 
-        $obj = "" | Select-Object -Property "ComputerName","AntiVirus-Product","AntiVirus-Status","AntiVirus-LastUpdate","OnAccessProtection-Status","RealTimeProtection-Status","BehaviorMonitor-Status","OfficeProtection-Status","NIS-Status","AntiMalware-Status","AM-RecentDetections","AntiSpyware-Product","AntiSpyware-Status","AntiSpyware-LastUpdate","Firewall-Product","Firewall-DomainProfileStatus"
+        [uint32]$HKLM = 2147483650
+        $obj = "" | Select-Object -Property "ComputerName","AntiVirus-Product","AntiVirus-Status","AntiVirus-LastUpdate","AntiVirus-Exclusions","OnAccessProtection-Status","RealTimeProtection-Status","BehaviorMonitor-Status","OfficeProtection-Status","NIS-Status","AntiMalware-Status","AM-RecentDetections","AntiSpyware-Product","AntiSpyware-Status","AntiSpyware-LastUpdate","Firewall-Product","Firewall-DomainProfileStatus"
         $obj.'ComputerName' = $ComputerName
     }
 
     PROCESS {
         # Get antimalware status
         try {
-            $antiMalwareStatus = Get-CimInstance -Namespace ROOT\Microsoft\SecurityClient -Class AntimalwareHealthStatus -CimSession $cimSession -ErrorAction Stop -Verbose:$false
-            if ($antiMalwareStatus -ne $null) {
+            $antiMalwareStatus = Get-CimInstance -Namespace ROOT\Microsoft\SecurityClient -ClassName AntimalwareHealthStatus -CimSession $cimSession -ErrorAction Stop -Verbose:$false
+            if ($antiMalwareStatus) {
                 $obj.'AntiVirus-Status' = $antiMalwareStatus.AntivirusEnabled
                 $obj.'AntiVirus-LastUpdate' = $antiMalwareStatus.AntivirusSignatureUpdateDateTime
                 $obj.'AntiSpyware-LastUpdate' = $antiMalwareStatus.AntispywareSignatureUpdateDateTime
@@ -94,7 +95,7 @@ function Get-SecurityStatus {
         # Get infection status
         try {
             $infection = New-Object System.Collections.ArrayList
-            $detections = Get-CimInstance -Namespace ROOT\Microsoft\SecurityClient -Class Malware -CimSession $cimSession -ErrorAction Stop -Verbose:$false
+            $detections = Get-CimInstance -Namespace ROOT\Microsoft\SecurityClient -ClassName Malware -CimSession $cimSession -ErrorAction Stop -Verbose:$false
             foreach ($detection in $detections) {
                 $properties = [ordered]@{
                     DetectionTime = $detection.DetectionTime
@@ -113,33 +114,70 @@ function Get-SecurityStatus {
         # If the host is a workstation, get details about security products
         $osDetails = Get-CimInstance -ClassName Win32_OperatingSystem -CimSession $cimSession -Verbose:$false
         if ($osDetails.ProductType -eq 1) {
-            $antiSpywareProduct = Get-CimInstance -Namespace ROOT/SecurityCenter2 -Class AntiSpywareProduct -CimSession $cimSession -Verbose:$false
-            if ($antiSpywareProduct -ne $null) {
+            if ($osDetails.Version[0] -lt 6) {
+                $namespace = "ROOT/SecurityCenter"
+            }
+            else {
+                $namespace = "ROOT/SecurityCenter2"
+            }
+            $antiSpywareProduct = Get-CimInstance -Namespace $namespace -ClassName AntiSpywareProduct -CimSession $cimSession -Verbose:$false
+            if ($antiSpywareProduct) {
                 $obj.'AntiSpyware-Product' = $antiSpywareProduct.displayName
             }
-            $antiVirusProduct = Get-CimInstance -Namespace ROOT/SecurityCenter2 -Class AntiVirusProduct -CimSession $cimSession -Verbose:$false
-            if ($antiVirusProduct -ne $null) {
+            $antiVirusProduct = Get-CimInstance -Namespace $namespace -ClassName AntiVirusProduct -CimSession $cimSession -Verbose:$false
+            if ($antiVirusProduct) {
                 $obj.'AntiVirus-Product' = $antiVirusProduct.displayName
             }
-            $firewallProduct = Get-CimInstance -Namespace ROOT/SecurityCenter2 -Class FirewallProduct -CimSession $cimSession -Verbose:$false
-            if ($firewallProduct -ne $null) {
+            $firewallProduct = Get-CimInstance -Namespace $namespace -ClassName FirewallProduct -CimSession $cimSession -Verbose:$false
+            if ($firewallProduct) {
                 $obj.'Firewall-Product' = $firewallProduct.displayName
             }
+
+            # Get the exclusions if available
+            $defenderPaths = @{
+                ExcludedPaths = 'SOFTWARE\Microsoft\Windows Defender\Exclusions\Paths\'
+                ExcludedExtensions = 'SOFTWARE\Microsoft\Windows Defender\Exclusions\Extensions\'
+                ExcludedProcesses = 'SOFTWARE\Microsoft\Windows Defender\Exclusions\Processes\'
+            }
+            $mcAfeePaths = @{
+                Exclusions = 'SOFTWARE\McAfee\AVSolution\OAS\DEFAULT\'
+                EmailIncludedProcesses = 'SOFTWARE\McAfee\AVSolution\OAS\EMAIL\'
+                ProcessStartupExclusions = 'SOFTWARE\McAfee\AVSolution\HIP\'
+            }
+            $exclusions = [PSCustomObject] @{}
+            if($obj.'AntiVirus-Product' -match 'Windows Defender') {
+                $defenderPaths.GetEnumerator() | ForEach-Object {
+                    if ($exclusion = $(Invoke-CimMethod -Class 'StdRegProv' -Name EnumValues -Arguments @{hDefKey=$HKLM; sSubKeyName=$($_.Value)} -CimSession $cimSession -Verbose:$false).sNames) {
+                        $exclusions | Add-Member -NotePropertyName $_.Key -NotePropertyValue $($exclusion -join ', ')
+                    }
+                }
+
+            }
+            elseif($obj.'AntiVirus-Product' -match 'McAfee') {
+                $mcAfeePaths.GetEnumerator() | ForEach-Object {
+                    if ($exclusion = $(Invoke-CimMethod -Class 'StdRegProv' -Name EnumValues -Arguments @{hDefKey=$HKLM; sSubKeyName=$($_.Value)} -CimSession $cimSession -Verbose:$false).sNames) {
+                        $exclusions | Add-Member -NotePropertyName $_.Key -NotePropertyValue $($exclusion -join ', ')
+                    }
+                }
+            }
+            $obj.'AntiVirus-Exclusions' = $exclusions
         }
 
         # Get firewall status
-        [uint32]$hive = 2147483650
         $key = 'SYSTEM\\CurrentControlSet\\Services\\SharedAccess\\Parameters\\FirewallPolicy\\DomainProfile'
         $value = 'EnableFirewall'
         $firewallDomainProfileStatus = (Invoke-CimMethod -ClassName StdRegProv -Name GetDWordValue -Arguments @{
-            hDefKey = $hive
+            hDefKey = $HKLM
             sSubKeyName = $key
             sValueName = $value
         } -CimSession $cimSession -Verbose:$false).uValue
-        if ($firewallDomainProfileStatus -ne $null) {
-            $obj.'Firewall-DomainProfileStatus' = $firewallDomainProfileStatus
+        if ($firewallDomainProfileStatus -eq 1) {
+            $firewallStatus = $True
         }
-
+        elseif ($firewallDomainProfileStatus -eq 0) {
+            $firewallStatus = $False
+        }
+        $obj.'Firewall-DomainProfileStatus' = $firewallStatus
         Write-Output $obj
     }
 

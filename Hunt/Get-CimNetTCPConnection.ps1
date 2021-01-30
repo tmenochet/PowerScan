37c17f4aa099b55c87a9e3edf8df32aba426c6c8
tmenochet <tmenochet@protonmail.com>
2021-01-30 11:27:21 +0100

    Added Get-CimNetTCPConnection.ps1

---
+++ Hunt/Get-CimNetTCPConnection.ps1
@@ -0,0 +1,161 @@
+#requires -version 3
+
+function Get-CimNetTCPConnection {
+<#
+.SYNOPSIS
+    Get current TCP connections on a remote computer.
+    Privileges required: high
+
+    Author: Timothée MENOCHET (@_tmenochet)
+
+.DESCRIPTION
+    Get-CimNetTCPConnection queries remote host through WMI for current TCP connections (optionally matching criteria).
+
+.PARAMETER ComputerName
+    Specifies the host to query.
+
+.PARAMETER Credential
+    Specifies the privileged account to use.
+
+.PARAMETER Ping
+    Ensures host is up before run.
+
+.PARAMETER Protocol
+    Specifies the protocol to use, defaults to DCOM.
+
+.PARAMETER RemoteAddress
+    Specifies one or more connections by remote address.
+
+.PARAMETER RemotePort
+    Specifies one or more connections by remote port.
+
+.PARAMETER State
+    Specifies one or more connections by state.
+
+.PARAMETER OwningProcess
+    Specifies one or more connections by owning process.
+
+.EXAMPLE
+    PS C:\> Get-CimNetTCPConnection -ComputerName SRV.ADATUM.CORP -Credential ADATUM\Administrator -RemoteAddress 192.168.1.2 -RemotePort 80,443 -State Established
+#>
+
+    [CmdletBinding()]
+    Param (
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ComputerName = $env:COMPUTERNAME,
+
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+        [Switch]
+        $Ping,
+
+        [ValidateSet('Dcom', 'Wsman')]
+        [String]
+        $Protocol = 'Dcom',
+
+        [ValidateNotNullOrEmpty()]
+        [string[]]
+        $RemoteAddress,
+
+        [ValidateNotNullOrEmpty()]
+        [int[]]
+        $RemotePort,
+
+        [ValidateSet('Bound', 'Closed', 'CloseWait', 'Closing', 'DeleteTCB', 'Established', 'FinWait1', 'FinWait2', 'LastAck', 'Listen', 'SynReceived', 'SynSent', 'TimeWait')]
+        [string]
+        $State,
+
+        [ValidateNotNullOrEmpty()]
+        [uint32]
+        $OwningProcess
+    )
+
+    BEGIN {
+        if ($Ping -and -not $(Test-Connection -Count 1 -Quiet -ComputerName $ComputerName)) {
+            Write-Verbose "[$ComputerName] Host is unreachable."
+            break
+        }
+
+        $cimOption = New-CimSessionOption -Protocol $Protocol
+        try {
+            if ($Credential.Username) {
+                $cimSession = New-CimSession -ComputerName $ComputerName -Credential $Credential -SessionOption $cimOption -ErrorAction Stop -Verbose:$false
+            }
+            else {
+                $cimSession = New-CimSession -ComputerName $ComputerName -SessionOption $cimOption -ErrorAction Stop -Verbose:$false
+            }
+        }
+        catch [Microsoft.Management.Infrastructure.CimException] {
+            Write-Verbose "[$ComputerName] Failed to establish CIM session."
+            break
+        }
+
+        $states = @{
+            'Bound' = '100'
+            'Closed' = '1'
+            'CloseWait' = '8'
+            'Closing' = '9'
+            'DeleteTCB' = '12'
+            'Established' =  '5'
+            'FinWait1' = '6'
+            'FinWait2' = '7'
+            'LastAck' = '10'
+            'Listen' = '2'
+            'SynReceived' = '4'
+            'SynSent' = '3'
+            'TimeWait' = '11'
+        }
+
+        $filters = New-Object System.Collections.ArrayList
+        switch ($PSBoundParameters.Keys) {
+            "RemoteAddress" {
+                $f = @()
+                foreach ($address in $RemoteAddress) {
+                    $f += "RemoteAddress = '$address'"
+                }
+                $filters.Add(($f -join ' OR ')) | Out-Null
+            }
+            "RemotePort" {
+                $f = @()
+                foreach ($port in $RemotePort) {
+                    $f += "RemotePort = '$port'"
+                }
+                $filters.Add(($f -join ' OR ')) | Out-Null
+            }
+            "State" {
+                $filters.Add("State = '$($states[$State])'") | Out-Null
+            }
+            "OwningProcess" {
+                $filters.Add("OwningProcess = '$OwningProcess'") | Out-Null
+            }
+            Default {}
+        }
+        $filter = $null
+        if ($filters.Count) {
+            $filter = $filters -join ' AND '
+        }
+    }
+
+    PROCESS {
+        Get-CimInstance -Namespace 'ROOT/StandardCimv2' -ClassName MSFT_NetTCPConnection -Filter $filter -CimSession $cimSession -Verbose:$false | ForEach-Object {
+            $obj = New-Object -TypeName psobject
+            $obj | Add-Member -MemberType NoteProperty -Name 'ComputerName' -Value $ComputerName
+            $obj | Add-Member -MemberType NoteProperty -Name 'LocalAddress' -Value $_.LocalAddress
+            $obj | Add-Member -MemberType NoteProperty -Name 'LocalPort' -Value $_.LocalPort
+            $obj | Add-Member -MemberType NoteProperty -Name 'RemoteAddress' -Value $_.RemoteAddress
+            $obj | Add-Member -MemberType NoteProperty -Name 'RemotePort' -Value $_.RemotePort
+            $obj | Add-Member -MemberType NoteProperty -Name 'State' -Value $_.State
+            $obj | Add-Member -MemberType NoteProperty -Name 'OwningProcess' -Value $_.OwningProcess
+            $obj | Add-Member -MemberType NoteProperty -Name 'CreationTime' -Value $_.CreationTime
+            Write-Output $obj
+        }
+    }
+
+    END {
+        Remove-CimSession -CimSession $cimSession
+    }
+}
